15db8f5f7ecda49f00714472bc27604b06280441
Rikx08 <73607438+Rikx08@users.noreply.github.com>
2025-03-27 13:48:27 +0300

    доработка main css + html

--- Ricord/account/forms.py
+++ Ricord/account/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from .models import Profile
+
 class RegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
 
@@ -9,7 +9,3 @@ class RegisterForm(UserCreationForm):
         model = User
         fields = ['username', 'email', 'password1', 'password2']
 
-class AvatarUploadForm(forms.ModelForm):
-    class Meta:
-        model = Profile
-        fields = ['avatar']
--- Ricord/main/views.py
+++ Ricord/main/views.py
@@ -3,12 +3,6 @@ from django.shortcuts import render
 def main(request):
     return render(request,"main/main.html")
 
-# @login_required
-# def profile(request):
-#     return render(request,"main/profile.html")
-from django.shortcuts import render
-from account.models import Profile
-
+@login_required
 def profile(request):
-    user_profile = Profile.objects.get(user=request.user)  # Получаем профиль текущего пользователя
-    return render(request, "profile.html", {"profile": user_profile})
+    return render(request,"main/profile.html")
