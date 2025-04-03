6d411662708feea272a5884e746ef679c0d02059
Rikx08 <73607438+Rikx08@users.noreply.github.com>
2025-04-03 14:29:31 +0300

    Добавлен .env в .gitignore

--- Ricord/Ricord/settings.py
+++ Ricord/Ricord/settings.py
@@ -125,6 +125,13 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'Drontop161532@gmail.com'
+EMAIL_HOST_PASSWORD = 'your_app_password'
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
